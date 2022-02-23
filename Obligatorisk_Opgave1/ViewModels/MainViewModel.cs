b80c91f0b08f96139a778a683abbba4a7de9c3cb
Jonas Henckel Phørcksen Klausen <10687@student.eadania.dk>
2022-02-23 10:09:31 +0100

    Window

--- Obligatorisk_Opgave1/ViewModels/MainViewModel.cs
+++ Obligatorisk_Opgave1/ViewModels/MainViewModel.cs
@@ -3,10 +3,44 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
 
 namespace Obligatorisk_Opgave1.ViewModels
 {
-    class MainViewModel
+    public class MainViewModel
     {
+
+    }
+    public class RelayCommand : ICommand
+    {
+        readonly Action<object> execute;
+        readonly Predicate<object> canExecute;
+
+        public RelayCommand(Action<object> execute)
+          : this(execute, null)
+        {
+        }
+
+        public RelayCommand(Action<object> execute, Predicate<object> canExecute)
+        {
+            this.execute = execute;
+            this.canExecute = canExecute;
+        }
+
+        public bool CanExecute(object parameter) //Return true hvis delegate unassigned, ellers håndtere event handler 
+        {
+            return canExecute == null ? true : canExecute(parameter);
+        }
+
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public void Execute(object parameter)//Invoke delegate
+        {
+            execute(parameter);
+        }
     }
 }

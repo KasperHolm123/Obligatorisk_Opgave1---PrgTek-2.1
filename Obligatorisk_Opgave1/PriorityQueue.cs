b8ef01ab694fa0e342eeb71c290d168431af460d
Jonas Henckel Phørcksen Klausen <10687@student.eadania.dk>
2022-02-23 11:00:29 +0100

    OnCollChanged

--- Obligatorisk_Opgave1/PriorityQueue.cs
+++ Obligatorisk_Opgave1/PriorityQueue.cs
@@ -2,15 +2,18 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Obligatorisk_Opgave1
 {
-    public class PriorityQueue<T> : IEnumerable<T> where T : IComparable<T>
+    public class PriorityQueue<T> : INotifyCollectionChanged, IEnumerable<T> where T : IComparable<T>
     {
         private LinkedList<T> _list = new LinkedList<T>();
+
+        public event NotifyCollectionChangedEventHandler CollectionChanged;
 
         public void Enqueue(T item)
         {
@@ -36,6 +39,7 @@ namespace Obligatorisk_Opgave1
                     _list.AddBefore(current, item);
                 }
             }
+            OnCollectionChanged("Add");
         }
 
         public T Dequeue()
@@ -48,7 +52,7 @@ namespace Obligatorisk_Opgave1
             T value = _list.First.Value;
 
             _list.RemoveFirst();
-
+            OnCollectionChanged("Remove");
             return value;
         }
 
@@ -61,7 +65,17 @@ namespace Obligatorisk_Opgave1
 
             return _list.First.Value;
         }
+        private void OnCollectionChanged(string operation)
+        {
+            if(CollectionChanged != null)
+            {
+                if (operation == "Add")
+                    CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Add));
 
+                else
+                    CollectionChanged(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Remove));
+            }
+        }
         public void Clear()
         {
             _list.Clear();

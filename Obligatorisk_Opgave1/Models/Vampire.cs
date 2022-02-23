0404ed768806ab9be2d931cfec5b03fe58bcd832
Kasper Holm <10669@student.eadania.dk>
2022-02-23 09:53:28 +0100

    Vampire class

---
+++ Obligatorisk_Opgave1/Models/Vampire.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_Opgave1.Models
+{
+    public class Vampire
+    {
+        public int BloodCurrency { get; set; }
+        public int Priority { get; set; }
+        public string Name { get; set; }
+
+        public Vampire(int bCurrency, int priority, string name)
+        {
+            BloodCurrency = bCurrency;
+            Priority = priority;
+            Name = name;
+        }
+    }
+}

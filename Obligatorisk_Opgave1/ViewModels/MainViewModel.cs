0ba7a629e1b7450907d7b366bfe56797118179d8
Jonas Henckel Phørcksen Klausen <10687@student.eadania.dk>
2022-02-23 09:55:03 +0100

    ViewModel

---
+++ Obligatorisk_Opgave1/ViewModels/MainViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_Opgave1.ViewModels
+{
+    class MainViewModel
+    {
+    }
+}

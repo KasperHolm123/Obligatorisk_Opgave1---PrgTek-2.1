d4b6e7bad97e4a18469c8be6459d065559b63fea
Jonas Henckel Phørcksen Klausen <10687@student.eadania.dk>
2022-02-24 09:54:42 +0100

    Interface

---
+++ Obligatorisk_Opgave1/IPriorityQueue.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Obligatorisk_Opgave1
+{
+    public interface IPriorityQueue<T> :IEnumerable<T>
+    {
+        void Enqueue(T elem);
+        void Enqueue(T elem, int p);
+        T Dequeue();
+        T Peek();
+        void Clear();
+        int Count { get; }
+        T[] ToArray();
+    }
+}
